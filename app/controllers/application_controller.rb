c992ba330e0187db051ecc5769f00957703322ba
Matthieu Martinot <leproudeur@yahoo.fr>
2017-03-29 06:19:28 +0200

    Bug username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
       :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password,
       :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar) }
-    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 
 end
