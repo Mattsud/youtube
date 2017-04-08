8bd7c5c2afef0d654dc49ae70075281d29574fa8
Matthieu Martinot <leproudeur@yahoo.fr>
2017-04-07 20:51:18 -0400

    vote favorite videos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.friendly.find(params[:id])
-    @favorite_videos = @user.find_voted_items
+    @favorite_videos = @user.find_voted_items.order(:cached_votes_up => :desc)
+
   end
 end
